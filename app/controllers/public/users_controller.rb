d41066c3d889aee012efecd389f088297860ed16
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-07-07 14:35:55 +0000

    Admin_and_User_1

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,37 @@
+class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:update, :edit]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "変更が保存されました。"
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @user = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image, :email)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions",
+  }
+
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions",
+  }
+
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+
+  scope module: :public do
+    resources :users, only: [:index, :show, :edit,:update]
+  end
+
 end
