068b560e29faae85714980f182a10641d3e17f49
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-07-11 13:53:51 +0000

    layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  GUEST_USER_EMAIL = "guest@example.com"
-  def self.guest
-    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
-    end
-  end
-  def not_guest_user?
-    email != GUEST_USER_EMAIL
-  end
-
   has_many :posts, dependent: :destroy
 
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+  GUEST_USER_EMAIL = "guest@example.com"
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "Guestuser"
+      user.introduction = "ShareSphereへようこそ"
+    end
+  end
+  def not_guest_user?
+    email != GUEST_USER_EMAIL
+  end
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
