b4c363673a9c4d44ed7c9a0ffc0bd8c6af45f7ba
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-07-26 15:05:41 +0000

    design_726

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
   def top
+    @post = Post.all.sample
+    @user = @post.user
+    @tags = Tag.order("RANDOM()").limit(5)
   end
 
   def about
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -20,7 +20,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(user), notice: "ゲストでログインしました"
+    redirect_to root_path, notice: "ゲストでログインしました"
   end
   # protected
 
