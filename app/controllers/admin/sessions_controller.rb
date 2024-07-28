2b8c1933818fc6428acac5441d41501ad014f22b
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-07-28 14:08:48 +0000

    error_fixed_728_4

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,9 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
