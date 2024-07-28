71400f5f4d51789e583ca9e884c5e11acd166a26
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-07-28 08:05:11 +0000

    error_fixed_728_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,18 @@
-Admin.create!(
+Admin.find_or_create_by!(
    email: 'admin@example.com',
    password: '123abc'
+)
+User.find_or_create_by!(
+  [
+   {email: 'mary@example.com', name: 'Mary', password: '123456'},
+   {email: 'lily@example.com', name: 'Lily', password: '123456'},
+   {email: 'lucy@example.com', name: 'Lucy', password: '123456'},
+   {email: 'jack@example.com', name: 'Jack', password: '123456'},
+   {email: 'Jam@example.com', name: 'Jam', password: '123456'},
+   {email: 'david@example.com', name: 'David', password: '123456'},
+   {email: 'leo@example.com', name: 'Leo', password: '123456'},
+   {email: 'tony@example.com', name: 'Tony', password: '123456'},
+   {email: 'lisa@example.com', name: 'Lisa', password: '123456'},
+   {email: 'daisy@example.com', name: 'Daisy', password: '123456'},
+  ]
 )
