375ef432bc5768da8092930c9c30a9b4e6a08df2
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-08-10 10:35:56 +0000

    pagination_fixed

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all.page(params[:page]).per(6)
+    @users = User.all.page(params[:page]).per(7)
   end
 
   def show
